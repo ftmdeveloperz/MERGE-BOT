b09e70a400410281d44dd437538494e2ff1156a2
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-03 15:18:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7270862829:AAFY2KNsTk2MnU2xrOvHochDsrdnyOkFW98")
     TELEGRAM_API = os.environ.get("TELEGRAM_API", "")
-    OWNER = os.environ.get("OWNER", "7042535787")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ftmdeveloperz")
-    PASSWORD = os.environ.get("PASSWORD", "@ftmdeveloperz")
+    OWNER = os.environ.get("OWNER", "7711039923")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ftmdeveloper")
+    PASSWORD = os.environ.get("PASSWORD", "@ftmdeveloper")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ftm:ftm@cluster0.rhh9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOGCHANNEL = os.environ.get("LOGCHANNEL", "")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
