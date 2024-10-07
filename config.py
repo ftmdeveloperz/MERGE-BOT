145b950d788493cb6527282b9b9ae303cfe57387
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-07 18:08:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     OWNER = os.environ.get("OWNER", "7711039923")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ftmdeveloper")
     PASSWORD = os.environ.get("PASSWORD", "@ftmbotz")
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ftm:ftm@cluster0.rhh9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ftm:<db_password>@cluster0.sowne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOGCHANNEL = os.environ.get("LOGCHANNEL", "-1002479719245")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
